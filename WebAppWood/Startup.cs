befe49836810c6c4c9169d63886dd47a0b0a542d
Chan <Chan@CHAN-PC>
2018-05-04 15:53:48 -0600

    Add project files.

---
+++ WebAppWood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppWood.Startup))]
+namespace WebAppWood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
